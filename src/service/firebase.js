9de3cdf76dceb6e4caee9da6421d53c128466b41
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2021-11-03 17:20:20 +0900

    Add env file

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -2,11 +2,10 @@ import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
   // 있는 그대로입니당
-  apiKey: 'AIzaSyAVy3BJCW7Twf_cnPAzKDmaLHrp51GcFGM',
-  authDomain: 'business-card-maker-8fbe8.firebaseapp.com',
-  projectId: 'business-card-maker-8fbe8',
-  storageBucket: 'business-card-maker-8fbe8.appspot.com',
-  appId: '1:432579266773:web:4dee957051a0566a807a5c'
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  appId: process.env.REACT_APP_FIREBASE_APPID
 }
 
 // Initialize Firebase
